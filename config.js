f9d2cf6d9ffaf2e27d343d753fa8d22cfc1e40f7
NDVvV <viet.nguyenduc.shust@gmail.com>
2023-03-08 00:36:18 +0700

    vietnd

---
+++ config.js
@@ -0,0 +1,21 @@
+const nameGirl = "Một nửa thế giới";
+const giftUrl = "";
+const eventName = "Chúc Mừng 8-3";
+const titleCard = "Một nửa quả địa cầu";
+const contentCard = [
+  "Chúc bạn nhận được nhiều quà, nhiều hoa nhiều lời khen và lời chúc của phái nam trong ngày hôm nay. Chúc bạn gặp nhiều may mắn, hạnh phúc, niềm vui và những điều tuyệt diệu trong cuộc sống!",
+  "Một ngày tràn ngập hoa hồng và quà gửi đến những người phụ nữ xinh đẹp tuyệt vời! Chúc cho những bông hồng mãi luôn tươi tắn, khỏe khoắn, xinh xắn và luôn thành công, thành công hơn nữa ạ.",
+  "Cầu chúc tất cả phụ nữ trên thế giới này mãi xinh đẹp, luôn là bông hoa thơm trong vườn hoa ngát hương. Và mỗi người trên con đường kiếm tìm hạnh phúc sẽ sớm được như ý!",
+  "Nhân dịp 8/3 tôi xin gửi tới tất cả phụ nữ trên toàn thế giới những lời chúc mừng tốt đẹp nhất! Chúc cho phụ nữ ngày càng xinh đẹp thành công hơn trong cuộc sống!",
+  "Chúc tất cả cá chị em phụ nữ, những người tôi quen và những người phụ nữ khắp mọi nơi trên thế giới này không còn vất vả, luôn có một cuộc sống bình an và mãi mãi hạnh phúc!",
+  "Chúc bạn từ hôm nay sẽ xinh đẹp hơn bao ngày trước đây và cứ đẹp hoài không nghỉ ngơi!"
+];
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "mung83.png";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
+const giftImage1 = "bg.png";
